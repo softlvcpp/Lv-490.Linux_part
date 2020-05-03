1068cb1852573583d825558676fcfdc3ce12aaf8
guard252 <danylotv43@gmail.com>
2020-05-03 14:49:50 +0300

    Created CLion project for server

---
+++ Server/cmake-build-debug/CMakeFiles/Server.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/danylo/LV490/Lv-490.Linux_part/Server/src/main.cpp" "/home/danylo/LV490/Lv-490.Linux_part/Server/cmake-build-debug/CMakeFiles/Server.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
